7af079985ce20367bc6a30f4684afb6c014b5513
Sush-psusmitha <sushsusmitha13@gmail.com>
2025-05-20 12:57:00 +0530

    'test'

---
+++ main.js
@@ -0,0 +1,79 @@
+
+
+
+
+// arr = [2,4,6,8,7,9,11]; 
+
+// find the sum in a non functional way 
+// const res = arr.reduce(function(acc,curr){
+//     if(curr > acc){
+//         acc = curr[i];
+//     }
+//     return acc
+// },curr[i]);
+// console.log(res);
+
+// function findSum(arr){
+//     let sum = 0; 
+//     for(let i = 0; i < arr.length; i++){
+//         sum = sum + arr[i]; 
+//     }
+//     return sum
+// }
+// const output =  findSum(arr);
+// console.log(output);
+
+
+// function findMax(arr){
+//     let max = 0; 
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] > max){
+//             max = arr[i];
+//         }
+//     }  return max
+// }
+// console.log(findMax(arr)) ;
+
+// const re = arr.reduce(function(acc,curr){
+//     if(curr > acc){
+//         acc = curr;
+//     }
+//     return acc;
+// },0);
+// console.log(re)
+
+// function findMin(arr){
+//     let min = arr[0]; 
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] < min){
+//             min = arr[i];
+//         }
+//     }return min;
+// }
+// console.log(findMin(arr));
+
+// const minVal = arr.reduce(function(acc,curr){
+//     if(curr < acc){  
+//         acc = curr;
+        
+//     }return acc;
+// },arr[0])
+
+// console.log(minVal);
+
+
+
+// filter
+// function isOdd(x){
+//     return x % 2 !== 0;
+// }
+// const output = arr.filter(isOdd); 
+// console.log(output)
+
+
+// const doubleArr = arr.map(x=>x*2); 
+// console.log(doubleArr); 
+// const tripple = arr.map(x=>x*3); 
+// console.log(tripple); 
+// const binary = arr.map(x=>x.toString(2)); 
+// console.log(binary)
